b3d562b91bf6ec39d02c07ce33a2ebe5dbf9286e
Adetunji Aramide <adetunjiaramide@gmail.com>
2025-03-08 06:44:48 +0100

    removed forced redirect from backend

--- src/functions/getShortenUrl.ts
+++ src/functions/getShortenUrl.ts
@@ -22,13 +22,7 @@ export const handler = async (event: APIGatewayProxyEvent) => {
 
         const originalUrl = record.originalUrl;
 
-        return formatJSONResponse({ 
-            data: {},
-            statusCode: 301,
-            headers: {
-                Location: originalUrl
-            }
-        })
+        return formatJSONResponse({ data: { originalUrl}})
 
     } catch(error:any){
         console.log('error', error)
